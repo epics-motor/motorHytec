17b57f757a9174c014583889587b937d751990c7
kpetersn <kmpeters@anl.gov>
2019-04-04 16:38:56 -0500

    Minor improvements to the example IOC

--- iocs/hytecIOC/iocBoot/iocHytec/st.cmd
+++ iocs/hytecIOC/iocBoot/iocHytec/st.cmd
@@ -2,7 +2,7 @@
 
 ## The following is needed if your board support package doesn't at boot time
 ## automatically cd to the directory containing its startup script
-#cd "/home/oxygen40/KPETERSN/epics/motor-split/Hytec/motorHytecBlank/iocs/hytecIOC/iocBoot/iocHytec"
+#cd "/home/username/epics/iocs/hytecIOC/iocBoot/iocHytec"
 
 < cdCommands
 #< ../nfsCommands
@@ -13,17 +13,25 @@ cd topbin
 ## everywhere it appears in this file
 ld 0,0, "hytec.munch"
 
-## Register all support components
 cd top
+
+## Register all support components
 dbLoadDatabase "dbd/hytec.dbd"
 hytec_registerRecordDeviceDriver pdbbase
 
-## Load record instances
-#dbLoadTemplate "db/hytec.substitutions"
-#dbLoadRecords "db/hytec.db", "user=kpetersn"
-
 cd startup
+
+## motorUtil (allstop & alldone)
+dbLoadRecords("$(MOTOR)/db/motorUtil.db", "P=hytec:")
+
+## IPAC carrier should be configured here
+
+## An example needs to be created based on README_Hy8601Asyn
+#!< Hytec8601.cmd
+
 iocInit
 
-## Start any sequence programs
-#seq &sncxxx, "user=kpetersn"
+## motorUtil (allstop & alldone)
+motorUtilInit("hytec:")
+
+# Boot complete
